379d60c4a53c4462b38d1fdbfd9c99c92d041fac
Zhongyi <zni573@connect.hkust-gz.edu.cn>
2025-05-28 20:54:08 +0800

    up docs

--- docs/make.jl
+++ docs/make.jl
@@ -3,21 +3,25 @@ using Documenter
 
 DocMeta.setdocmeta!(QECC, :DocTestSetup, :(using QECC); recursive=true)
 
-makedocs(;
+bib = CitationBibliography(joinpath(@__DIR__,"src/references.bib"),style=:authoryear)
+
+makedocs(
+    plugins = [bib],
     modules=[QECC],
     authors="nzy1997",
     sitename="QECC.jl",
     format=Documenter.HTML(;
-        canonical="https://nzy1997.github.io/QECC.jl",
+        canonical="https://QuantumSavory.github.io/QECC.jl",
         edit_link="main",
         assets=String[],
     ),
     pages=[
         "Home" => "index.md",
+        "References" => "references.md",
     ],
 )
 
 deploydocs(;
-    repo="github.com/nzy1997/QECC.jl",
+    repo="github.com/QuantumSavory/QECC.jl",
     devbranch="main",
 )
--- docs/src/index.md
+++ docs/src/index.md
@@ -3,12 +3,3 @@ CurrentModule = QECC
 ```
 
 # QECC
-
-Documentation for [QECC](https://github.com/nzy1997/QECC.jl).
-
-```@index
-```
-
-```@autodocs
-Modules = [QECC]
-```
